47115812788f2e894ab255caa43a390606a66c96
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-16 12:03:40 +0100

    Add button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+  // eslint-disable-next-line no-console
+  const clickHandler = () => console.log('clicked');
+
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button onClick={clickHandler}>Click me</button>
+  );
+};
+
+export default Button;
