d222298fa45ab1dfeba3b6aa915261340bc579f1
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-16 14:43:46 +0100

    Fix minor issues

--- src/components/ModeToggler.jsx
+++ src/components/ModeToggler.jsx
@@ -1,16 +1,14 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-console */
 /* eslint-disable react/button-has-type */
 import React from 'react';
 
 const ModeToggler = () => {
-  let darkModeOn = false;
+  let darkModeOn = true;
   const darkMode = <h1>Dark mode is on</h1>;
   const lightMode = <h1>Light mode is on</h1>;
 
   function handleClick() {
     darkModeOn = !darkModeOn;
-    if (darkModeOn == true) {
+    if (darkModeOn === true) {
       console.log('Dark mode is on');
     } else {
       console.log('Light mode is on');
