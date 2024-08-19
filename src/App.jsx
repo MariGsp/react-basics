bf57810e3436defaef8d856e9fd207bfdb0180c6
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-19 15:10:50 +0100

    Restore app to heading only

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import './App.css';
 
 // Component
 import Heading from './components/Heading';
-import RegisterForm from './components/RegisterForm';
 
 const App = () => {
   return (
     <div>
       <Heading />
-      <RegisterForm />
     </div>
   );
 };
