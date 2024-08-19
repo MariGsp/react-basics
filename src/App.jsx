5ca25d6b1dcc5d6d4ff237641b3318cd182cb1c9
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-19 15:31:23 +0100

    Create fresh app as blog page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 
 // Component
 import Heading from './components/Heading';
-import RegisterForm from './components/RegisterForm';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Heading />
-      <RegisterForm />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
     </div>
   );
 };
