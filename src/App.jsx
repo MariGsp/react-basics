fa31e160bf1c7d5cc287d25ff6bee14cc684eddc
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-19 15:15:04 +0100

    Create fresh app with cards only

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 
 // Component
-import Heading from './components/Heading';
-import RegisterForm from './components/RegisterForm';
+import Card from './components/Card';
 
 const App = () => {
   return (
     <div>
-      <Heading />
-      <RegisterForm />
+      <Card h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
     </div>
   );
 };
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="copyright">
-      <p>Made with love by myself</p>
-    </div>
-  );
-};
-
-export default Footer;
