c5634bd3fbf768fe5dcb9e59bc627e7bea637dcb
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-20 17:29:28 +0100

    Add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 
 // Component
-import Heading from './components/Heading';
+import { Routes, Route, Link } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import AboutMe from './components/AboutMe';
 
 const App = () => {
   return (
-    <div>
-      <Heading />
+    <div className="App">
+      <nav className="nav">
+        <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about-me" className="nav-item">About Me</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
+
     </div>
   );
 };
