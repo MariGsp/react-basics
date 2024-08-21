ea1ebe1baff6c0e8f35bb37bfb124a661ced651d
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-21 15:08:12 +0100

    Add fresh start for calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import ReactPlayer from 'react-player/youtube';
-import logo from './assets/images/Spongebob.png';
-
 const App = () => {
-  return (
-    <div>
-      <h1>Here is Spongebob!</h1>
-      <img src={logo} alt="Spongebob logo" />
-      <h1>And here is a video:</h1>
-      <MyVideo />
-    </div>
-  );
-};
-
-const MyVideo = () => {
-  return (
-    <ReactPlayer url="https://www.youtube.com/watch?v=iGGHKq8kXTs" playing={false} volume={0.5} />
-  );
+  return ();
 };
 
 export default App;
