bee5e6d3266d121a3fc18be894c0cfea7c659bb7
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-08-21 10:04:41 +0100

    Fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 
-// Component
-import { Routes, Route, Link } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import AboutMe from './components/AboutMe';
+// Components
 
 const App = () => {
-  return (
-    <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/about-me" className="nav-item">About Me</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-me" element={<AboutMe />} />
-      </Routes>
-
-    </div>
-  );
+  return ();
 };
 
 export default App;
